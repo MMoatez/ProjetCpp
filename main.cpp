2d3eb8f5e907733c9a50f6268840d407e7a784de
Moatez <moatez.mathlouthi@esprit.tn>
2023-10-25 23:06:40 +0200

    Second commit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 #include <string>
-#include "Compte.h"
+
 using namespace std;
 
 int main()
 {
-    Compte C, C1;
-    float val, somme;
 
-    C.saisir();
 
-    cout << "Quelle est la somme que vous voulez retirer : ";
-    cin >> val;
-    C.retirerArgent(val);
+    cout << "Hello MR MATHLOUTHI Moatez ";
 
-    cout << "Quelle est la quantité que vous voulez ajouter : ";
-    cin >> somme;
-    C.deposerArgent(somme);
-
-    C.transfererArgent(C1);
 
     return 0;
 }
